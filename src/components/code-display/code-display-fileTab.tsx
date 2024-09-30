bd9d32f7c3d66903e42cd3ceedae1ff56e20b1f8
WIsavachart <wisavachart.s@gmail.com>
2024-09-30 13:37:03 +0700

    Active Tab

--- src/components/code-display/code-display-fileTab.tsx
+++ src/components/code-display/code-display-fileTab.tsx
@@ -12,14 +12,20 @@ const FileTab = ({ edmNum, linkNum }: FileName) => {
             activeTab === FileTabs.DIGI && `border-bottom-filename-select`
           }`}
           onClick={() => setActiveTab(FileTabs.DIGI)}>
-          <h6 className="text-cs_3 text-[14px]">{`${edmNum}_${linkNum}_digi.html`}</h6>
+          <h6
+            className={`${
+              activeTab === FileTabs.DIGI ? `text-cs_3` : `text-cb_1`
+            } text-[14px]`}>{`${edmNum}_${linkNum}_digi.html`}</h6>
         </div>
         <div
           className={`py-1 cursor-pointer ${
             activeTab === FileTabs.TPS && `border-bottom-filename-select`
           }`}
           onClick={() => setActiveTab(FileTabs.TPS)}>
-          <h6 className="text-cb_1 text-[14px]">{`${edmNum}_${linkNum}_tps.html`}</h6>
+          <h6
+            className={`${
+              activeTab === FileTabs.TPS ? `text-cs_3` : `text-cb_1`
+            } text-[14px]`}>{`${edmNum}_${linkNum}_tps.html`}</h6>
         </div>
       </div>
     </div>
