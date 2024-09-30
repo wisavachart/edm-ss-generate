b056e67e77dbe07354e06d892be5b8886edf6295
WIsavachart <wisavachart.s@gmail.com>
2024-09-30 16:57:55 +0700

    Fix

--- src/components/sidebar-menu/SidebarMenuItem.tsx
+++ src/components/sidebar-menu/SidebarMenuItem.tsx
@@ -8,14 +8,14 @@ const SidebarMenuItem = ({ name, src }: SidebarMenuItemPROPS) => {
   const { selectedMenu, handleMenuClick } = useSetupMenu();
   return (
     <div
-      className="flex items-center gap-4 cursor-pointer text-ct_3 hover:text-cs_3"
+      className={`flex items-center gap-4 cursor-pointer ${
+        selectedMenu === name ? `text-cs_3` : `text-ct_3`
+      } ${selectedMenu !== name && `hover:text-ct_2`}`}
       onClick={() => handleMenuClick(name)}>
       <div className="flex icon-border  rounded-lg items-center justify-center h-[36px] w-[36px]">
         <img src={src} alt="" />
       </div>
-      <h6 className={selectedMenu === name ? `font-semibold` : `font-thin`}>
-        {name}
-      </h6>
+      <h6 className="font-normal">{name}</h6>
     </div>
   );
 };
